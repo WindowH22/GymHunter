903cf2e9f4feafefd9b1fdba88784d311c28e3f9
WindowH22 <dlckdgml1235@naver.com>
2023-04-06 22:58:26 +0900

    first commit

---
+++ src/main/java/com/gym/gymhunter/GymHunterApplication.java
@@ -0,0 +1,13 @@
+package com.gym.gymhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymHunterApplication.class, args);
+	}
+
+}
